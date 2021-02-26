4620dc0dc370bc16cc3f52f75d0d0eda9c8eff8d
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-02-26 04:58:04 +0000

    js file has been set

---
+++ app.js
@@ -0,0 +1,6 @@
+document.querySelector('.close-btn').addEventListener('click',function(){
+    document.querySelector('.sidebar').classList.toggle('show-sidebar')
+})
+document.querySelector('.sidebar-toggle').addEventListener('click',function(){
+    document.querySelector('.sidebar').classList.toggle('show-sidebar')
+})
